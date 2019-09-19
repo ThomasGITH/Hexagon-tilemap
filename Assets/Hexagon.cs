839490135b04153d0b58f6ca0f748a772e304072
Thomas Linssen <31830553+ThomasGITH@users.noreply.github.com>
2019-09-19 12:26:51 +0200

    Add files via upload

---
+++ Assets/Hexagon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    public Material material;
+
+    void Awake()
+    {
+        Vector3[] vertices = new Vector3[7];
+        Vector2[] uv = new Vector2[7];
+        int[] triangles = new int[18];
+
+        vertices[0] = new Vector3(0.25f, 0.0f);
+        vertices[1] = new Vector3(0.75f, 0.0f);
+        vertices[2] = new Vector3(0.5f, 0.5f);
+        vertices[3] = new Vector3(1.0f, 0.5f);
+        vertices[4] = new Vector3(0.75f, 1.0f);
+        vertices[5] = new Vector3(0.25f, 1.0f);
+        vertices[6] = new Vector3(0.0f, 0.5f);
+
+        uv[0] = new Vector2(0.25f, 0.0f);
+        uv[1] = new Vector2(0.75f, 0.0f);
+        uv[2] = new Vector2(0.5f, 0.0f);
+        uv[3] = new Vector2(1.0f, 0.5f);
+        uv[4] = new Vector2(0.75f, 1.0f);
+        uv[5] = new Vector2(0.25f, 1.0f);
+        uv[6] = new Vector2(0.0f, 0.5f);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        triangles[3] = 2;
+        triangles[4] = 1;
+        triangles[5] = 3;
+
+        triangles[6] = 3;
+        triangles[7] = 4;
+        triangles[8] = 2;
+
+        triangles[9] = 2;
+        triangles[10] = 4;
+        triangles[11] = 5;
+
+        triangles[12] = 2;
+        triangles[13] = 5;
+        triangles[14] = 6;
+
+        triangles[15] = 2;
+        triangles[16] = 6;
+        triangles[17] = 0;
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+
+        GameObject go = new GameObject("Hexagon", typeof(MeshFilter), typeof(MeshRenderer));
+        go.transform.position = new Vector2(-20,-20);
+        go.GetComponent<MeshFilter>().mesh = mesh;
+
+        go.GetComponent<MeshRenderer>().material = material;
+
+    }
+}
---
+++ Assets/Tilemap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilemap : MonoBehaviour
+{
+    public Vector2 position, size;
+
+    void Start()
+    {
+        GameObject hexagon = GameObject.Find("Hexagon");
+        
+        for (int i = 0; i < size.y; i++)
+        {
+            for (int j = 0; j < size.x; j++)
+            {
+                Instantiate(hexagon, new Vector2(position.x + (i % 2 == 0 ? 0 : 0.75f) + (1.50f * j),
+                    position.y + 0 - (0.52f * i)), transform.rotation);
+            }
+        }
+
+
+    }
+}
